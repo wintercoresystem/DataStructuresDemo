f9d2234ef9391dfff0b6b9f3b26362872a4231c0
wintercoresystem <wintercoresystem@gmail.com>
2024-04-03 22:22:41 +0300

    Added simple array list and all methods needed except sorting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SimpleArrayList array = new SimpleArrayList(3);
+        System.out.println("Hello");
+        array.add(1);
+        array.add(2);
+
+        array.printElements();
+//        System.out.println(Arrays.toString(array.getElements()));
+
     }
 }
---
+++ src/main/java/org/example/SimpleArrayList.java
@@ -0,0 +1,140 @@
+package org.example;
+
+public class SimpleArrayList {
+    int maxSize = 8; // Default array size
+    int[] elements;
+    int topElement = 0;
+
+    // Constructor for empty array
+    SimpleArrayList() {
+        this.elements = new int[maxSize];
+    }
+    // Constructor for already made array
+    SimpleArrayList(int[] array)  {
+        this.elements = array;
+        this.maxSize = array.length;
+        this.topElement = array.length;
+
+    }
+    // Constructor for n elements
+    SimpleArrayList(int newSize) {
+        this.elements = new int[newSize];
+        this.maxSize = newSize;
+    }
+
+
+    // Increase array size to avoid overflow
+    private void extendArray() {
+        this.maxSize *= 2;
+        int[] biggerArray = new int[this.maxSize];
+        for (int i = 0; i < this.elements.length; i++) {
+            biggerArray[i] = this.elements[i];
+        }
+
+        this.elements = biggerArray;
+    }
+
+    // Check if index is valid, throws error if not
+    private void checkIndex(int index) {
+        if (index > this.topElement - 1 || index < 0) {
+            throw new IllegalArgumentException("Select existing index");
+        }
+    }
+
+    private void shiftArray(int shiftFrom, boolean forward) {
+        // If adding
+        if (forward) {
+            for (int i = topElement; i > shiftFrom - 1; i--) {
+                this.elements[i + 1] = this.elements[i];
+            }
+        } else { // If removing
+            for (int i = shiftFrom; i < topElement + 1; i++) {
+                this.elements[i] = this.elements[i + 1];
+            }
+        }
+    }
+
+    // Append element in the end
+    public void add(int element) {
+        // TODO maybe make private?
+        if (this.topElement + 1 >= this.maxSize) {
+            extendArray();
+        }
+        this.elements[this.topElement] = element;
+
+        this.topElement += 1;
+    }
+
+    // Add element in any index
+    public void add (int element, int index) {
+        if (index >= this.topElement) {
+            this.add(element);
+            return;
+        }
+        this.checkIndex(index);
+        // TODO maybe make private?
+        if (this.topElement + 1 >= this.maxSize) {
+            extendArray();
+        }
+        shiftArray(index, true);
+        this.elements[index] = element;
+        this.topElement += 1;
+    }
+
+    // Add all elements from other array
+    public void addAll(int[] arrayToAdd) {
+        for (int i = 0; i < arrayToAdd.length; i++) {
+            this.add(arrayToAdd[i]);
+        }
+    }
+
+    public void addAll(SimpleArrayList simpleArrayList) {
+        int[] arrayToAdd = simpleArrayList.getElements();
+        addAll(arrayToAdd);
+    }
+
+    // Delete last element and return it
+    public int pop() {
+        int temp = this.elements[this.topElement];
+        this.topElement -= 1;
+        return temp;
+    }
+
+    public int remove(int index) {
+        this.checkIndex(index);
+        int temp = this.elements[index];
+
+        shiftArray(index, false);
+        this.topElement -= 1;
+        return temp;
+
+    }
+
+    // Prints every element
+    public void printElements() {
+        for (int i = 0; i < this.maxSize; i++) {
+            System.out.print(this.elements[i]);
+            if (i >= this.topElement) {
+                System.out.print(" < hidden");
+            }
+            System.out.println();
+        }
+    }
+
+
+    // Getters
+    public int[] getElements() {
+        int[] realArray = new int[this.topElement];
+        for (int i = 0; i < topElement; i++) {
+            realArray[i] = this.elements[i];
+        }
+
+        return realArray;
+    }
+
+    // Get element by index
+    public int get(int index) {
+        this.checkIndex(index);
+        return this.elements[index];
+    }
+}
